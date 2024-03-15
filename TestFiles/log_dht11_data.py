8973ea95316843e3d5f01163d3e9228398141ff2
vicgud123 <victor.emil.gudmundsen@gmail.com>
2024-03-15 09:18:37 +0100

    Update log_dht11_data.py

--- TestFiles/log_dht11_data.py
+++ TestFiles/log_dht11_data.py
@@ -1,7 +1,10 @@
 import sqlite3
 from random import randint
 from datetime import datetime
-from time import sleep
+import paho.mqtt.subscribe as subscribe
+import json
+
+
 
 def create_table():
     query = """CREATE TABLE IF NOT EXISTS stue (humidity REAL NOT NULL, datetime TEXT NOT NULL, temperature REAL NOT NULL);"""
@@ -17,26 +20,29 @@ def create_table():
     finally:
         conn.close()
 
-def log_stue_dht11():
-    while True:
-        query = """INSERT INTO stue(datetime, temperature, humidity) VALUES(?, ?, ?)"""
-        now = datetime.now()
-        now = now.strftime("%d/%m/%y %H:%M:%S")
-        data = (now, randint(0, 30), randint(0, 100))
-        try:
-            conn = sqlite3.connect("database/sensor_data.db")
-            cur = conn.cursor()
-            cur.execute(query, data)
-            conn.commit()
-        except sqlite3.Error as sql_e:
-            print(f"sqlite error occured: {sql_e}")
-            conn.rollback()
-        except Exception as e:
-            print(f"Error occured: {e}")
-        finally:
-            conn.close()
-        sleep(1)
-
-
-create_table()        
-log_stue_dht11()
+create_table()   
+     
+def on_message_print(client, userdata, message):
+    print("%s %s" % (message.topic, message.payload))
+    query = """INSERT INTO stue(datetime, temperature, humidity) VALUES(?, ?, ?)"""
+    now = datetime.now()
+    now = now.strftime("%d/%m/%y %H:%M:%S")
+    print(type(json.loads(message.payload.decode())))
+    dht11_data = json.loads(message.payload.decode())
+    data = (now, dht11_data['temperature'], dht11_data['humidity'])
+   
+   
+    try:
+        conn = sqlite3.connect("database/sensor_data.db")
+        cur = conn.cursor()
+        cur.execute(query, data)
+        conn.commit()
+    except sqlite3.Error as sql_e:
+        print(f"sqlite error occured: {sql_e}")
+        conn.rollback()
+    except Exception as e:
+        print(f"Error occured: {e}")
+    finally:
+        conn.close()
+print("script is running")
+subscribe.callback(on_message_print, "paho/test/topic", hostname="4.231.174.166", userdata={"message_count": 0})
