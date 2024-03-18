b055de1d7ae19f9ff0765db544f3ed8dac48fba5
vicgud123 <victor.emil.gudmundsen@gmail.com>
2024-03-18 16:01:09 +0100

    gg

--- Window/main.py
+++ Window/main.py
@@ -1,5 +1,5 @@
 from umqtt.simple import MQTTClient
-from machine import Pin
+from machine import Pin, Timer
 from time import sleep
 IN1 = Pin(21, Pin.OUT)
 IN2 = Pin(22, Pin.OUT)
@@ -8,13 +8,14 @@ IN4 = Pin(32, Pin.OUT)
 
 pins = [IN1, IN2, IN3, IN4]
 
+manuel_window = Timer(1)
 # Assuming a 4-wire stepper motor, we define sequences for 1 step
 # For a 2-wire stepper motor, you would only need 2 sequences
 sequence_forward = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
 sequence_backward = [[0,0,0,1],[0,0,1,0],[0,1,0,0],[1,0,0,0]]
 
-pb1 = Pin(4, Pin.IN, Pin.PULL_UP)
-pb2 = Pin(0, Pin.IN, Pin.PULL_UP)
+pb1 = Pin(4, Pin.IN)
+pb2 = Pin(0, Pin.IN)
 
 DEBOUNCE_DELAY = 50 # milliseconds
 
@@ -22,14 +23,8 @@ CLIENT_NAME = 'esp32_client'
 BROKER_ADDR = '4.231.174.166' 
 mqttc = MQTTClient(CLIENT_NAME, BROKER_ADDR, keepalive=5)
 
-def states():
-    window_closed = True
 
-def debounce(pin):
-    """Debounce a pin."""
-    initial_value = pin.value()
-    sleep(DEBOUNCE_DELAY / 1000.0) # Convert milliseconds to seconds
-    return pin.value() == initial_value
+window_closed = True
 
 def move_motor(steps, direction):
     """Move the motor a certain number of steps in a given direction."""
@@ -39,11 +34,21 @@ def move_motor(steps, direction):
             for i in range(len(pins)):
                 pins[i].value(step[i])
                 sleep(0.001)
+def Manual(manual_window):
+    move_motor(500, 'backward')
+    
 
 def sub_topic(topic, msg):
- print((topic, msg))
- if topic == b'window_command' and msg == b'received':
-     print('ESP received "" message')
+    global window_closed
+    print((topic, msg))
+    print(window_closed)
+    if topic == b'window_command' and msg == b'open' and window_closed == True:
+        move_motor(500, 'forward')
+        window_closed = False
+    if topic == b'window_command' and msg == b'close' and window_closed == False:
+        move_motor(500, 'backward')
+        window_closed = True  
+        
 def connect_and_subscribe():
     global client_id, mqtt_server, topic_sub1, topic_sub2, topic_sub3
     client = MQTTClient(client_id, mqtt_server)
@@ -65,18 +70,18 @@ try:
 except OSError as e:
     restart_and_reconnect()
     
-    
 while True:
-    try:
-        client.check_msg()
-        if (time.time() - last_message) > message_interval:
-            msg = b'Hello #%d' % counter
-            client.publish(topic_pub, msg)
-            last_message = time.time()
-            counter += 1
-    except OSError as e:
-        restart_and_reconnect()
-    if msg = b'open' and window_closed = True:
+    client.check_msg()
+    sleep(3)
+    first = pb1.value()
+    sleep(0.01)
+    second = pb1.value()
+    first2 = pb2.value()
+    sleep(0.01)
+    second2 = pb2.value()
+    if first  == 1 and second == 0:
+        print("1")
         move_motor(500, 'forward')
-        States.window_closed = False
-        
+        manual_window.init(period=6100, mode=Timer.ONE_SHOT, callback=Manual)
+    if first2 == 1 and first2 == 0: 
+        move_motor(500, 'backward') 
